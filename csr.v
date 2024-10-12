3e03628d4155d291a72bb3f0ecc171048637ac0b
baisl-man <1262305030@qq.com>
2024-10-12 20:43:21 +0800

    feat: 参考课本的csr.v

--- csr.v
+++ csr.v
@@ -42,15 +42,16 @@
 module csr(
     input  wire          clk       ,
     input  wire          reset     ,
-    // 读端口
+    // command access interface(指令访问接口)
     input  wire          csr_re    ,
     input  wire [13:0]   csr_num   ,
     output wire [31:0]   csr_rvalue,
-    // 写端口
     input  wire          csr_we    ,
     input  wire [31:0]   csr_wmask ,
     input  wire [31:0]   csr_wvalue,
-    // 与硬件电路交互的接口信号
+
+    /* interface signals for direct interaction with hardware circuits
+    (与硬件电路交互的接口信号) */
     output wire [31:0]   ex_entry  , //送往pre-IF的异常入口地址
     output wire [31:0]   ertn_entry, //送往pre-IF的返回入口地址
     output wire          has_int   , //送往ID阶段的中断有效信号
@@ -123,6 +124,7 @@ module csr(
     assign has_int = (|(csr_estat_is[11:0] & csr_ecfg_lie[11:0])) & csr_crmd_ie;
     assign ex_entry = csr_eentry_data;
     assign ertn_entry = csr_era_data;
+
     // CRMD的PLV、IE域
     always @(posedge clk) begin
         if (reset) begin
@@ -146,27 +148,19 @@ module csr(
     end
 
     // CRMD的DA、PG、DATF、DATM域
+    /*等到第九章的实践任务中可完善DA和PG域的功能，
+    等到第十章的实践任务中可进一步完善DATF和DATM域的功能。*/
     always @(posedge clk) begin
         if(reset) begin
             csr_crmd_da   <= 1'b1;
             csr_crmd_pg   <= 1'b0;
-            csr_crmd_datf <= 2'b0;
-            csr_crmd_datm <= 2'b0;
-        end
-        else if(csr_we &&  wb_ecode==`ECODE_TLBR) begin
-            csr_crmd_da   <= 1'b1;
-            csr_crmd_pg   <= 1'b1;
-        end
-        else if (csr_we && csr_estat_ecode == `ECODE_TLBR) begin
-            csr_crmd_da   <= 1'b0;
-            csr_crmd_pg   <= 1'b1;
-            csr_crmd_datf <= 2'b01;
-            csr_crmd_datm <= 2'b01;            
+            csr_crmd_datf <= 2'b00;
+            csr_crmd_datm <= 2'b00;
         end
     end
 
     // PRMD的PPLV、PIE域
-    always @(posedge clk) begin//未定义要复位
+    always @(posedge clk) begin
         if (wb_ex) begin
             csr_prmd_pplv <= csr_crmd_plv;
             csr_prmd_pie  <= csr_crmd_ie;
@@ -184,9 +178,10 @@ module csr(
         if(reset)
             csr_ecfg_lie <= 13'b0;
         else if(csr_we && csr_num == `CSR_ECFG)
-            csr_ecfg_lie <= csr_wmask[`CSR_ECFG_LIE] & csr_wvalue[`CSR_ECFG_LIE]
-                        |  ~csr_wmask[`CSR_ECFG_LIE] & csr_ecfg_lie;
+            csr_ecfg_lie <= csr_wmask[`CSR_ECFG_LIE] & 13'h1bff & csr_wvalue[`CSR_ECFG_LIE]
+                        |  ~csr_wmask[`CSR_ECFG_LIE] & 13'h1bff & csr_ecfg_lie;
     end
+
     // ESTAT的IS域
     assign hw_int_in = 8'b0;
     assign ipi_int_in= 1'b0;
@@ -194,13 +189,13 @@ module csr(
         if (reset) begin
             csr_estat_is[1:0] <= 2'b0;
         end
-        else if (csr_we && (csr_num == `CSR_ESTAT)) begin   //被csr更新
+        else if (csr_we && (csr_num == `CSR_ESTAT)) begin
             csr_estat_is[1:0] <= ( csr_wmask[`CSR_ESTAT_IS10] & csr_wvalue[`CSR_ESTAT_IS10])
                                | (~csr_wmask[`CSR_ESTAT_IS10] & csr_estat_is[1:0]          );
         end
 
-        csr_estat_is[9:2] <= hw_int_in[7:0]; //硬中断 引脚
-        csr_estat_is[10] <= 1'b0; //无定义
+        csr_estat_is[9:2] <= hw_int_in[7:0];
+        csr_estat_is[10] <= 1'b0;
 
         if (timer_cnt[31:0] == 32'b0) begin
             csr_estat_is[11] <= 1'b1;
@@ -208,7 +203,7 @@ module csr(
         else if (csr_we && csr_num == `CSR_TICLR && csr_wmask[`CSR_TICLR_CLR] 
                 && csr_wvalue[`CSR_TICLR_CLR]) 
             csr_estat_is[11] <= 1'b0;
-        csr_estat_is[12] <= ipi_int_in;     // 核间中断
+        csr_estat_is[12] <= ipi_int_in;
     end    
     // ESTAT的Ecode和EsubCode域
     // 触发异常时填写异常的类型代号，精确异常是在写回级进行触发
@@ -227,7 +222,15 @@ module csr(
             csr_era_data <= csr_wmask[`CSR_ERA_PC] & csr_wvalue[`CSR_ERA_PC]
                         | ~csr_wmask[`CSR_ERA_PC] & csr_era_data;
     end
-     // EENTRY
+    // BADV的VAddr域
+    //load store在执行级、访存级和写回级增加虚地址通路，采用增加一个vaddr域
+    assign wb_ex_addr_err = wb_ecode==`ECODE_ALE || wb_ecode==`ECODE_ADE; 
+    always @(posedge clk) begin
+        if (wb_ex && wb_ex_addr_err) begin
+            csr_badv_vaddr <= (wb_ecode==`ECODE_ADE && wb_esubcode==`ESUBCODE_ADEF) ? wb_pc:wb_vaddr;
+        end
+    end
+    // EENTRY
     always @(posedge clk) begin
         if (csr_we && (csr_num == `CSR_EENTRY))
             csr_eentry_va <=   csr_wmask[`CSR_EENTRY_VA] & csr_wvalue[`CSR_EENTRY_VA]
@@ -249,14 +252,7 @@ module csr(
             csr_save3_data <=  csr_wmask[`CSR_SAVE_DATA] & csr_wvalue[`CSR_SAVE_DATA]
                             | ~csr_wmask[`CSR_SAVE_DATA] & csr_save3_data;
     end
-    // BADV的VAddr域
-    //load store在执行级、访存级和写回级增加虚地址通路，采用增加一个vaddr域
-    assign wb_ex_addr_err = wb_ecode==`ECODE_ALE || wb_ecode==`ECODE_ADE; 
-    always @(posedge clk) begin
-        if (wb_ex && wb_ex_addr_err) begin
-            csr_badv_vaddr <= (wb_ecode==`ECODE_ADE && wb_esubcode==`ESUBCODE_ADEF) ? wb_pc:wb_vaddr;
-        end
-    end
+
     // TID
     always @(posedge clk) begin
         if (reset) begin
@@ -286,7 +282,7 @@ module csr(
 
     // TVAL的TimeVal域 返回定时器计数器的值
     assign tcfg_next_value = csr_wmask[31:0] & csr_wvalue[31:0]
-                           |~csr_wmask[31:0] & csr_tcfg_data;
+                           |~csr_wmask[31:0] & {csr_tcfg_initval, csr_tcfg_periodic, csr_tcfg_en};
     always @(posedge clk) begin
         if (reset) begin
             timer_cnt <= 32'hffffffff;
@@ -294,7 +290,7 @@ module csr(
         else if (csr_we && csr_num == `CSR_TCFG && tcfg_next_value[`CSR_TCFG_EN]) begin
             timer_cnt <= {tcfg_next_value[`CSR_TCFG_INITV], 2'b0};
         end
-        else if (csr_tcfg_en && timer_cnt != 32'hffffffff) begin //定时器是非周期性的所以如果 0-1=ff..ff,那么停止计数
+        else if (csr_tcfg_en && timer_cnt != 32'hffffffff) begin
             if (timer_cnt[31:0] == 32'b0 && csr_tcfg_periodic) begin
                 timer_cnt <= {csr_tcfg_initval, 2'b0};
             end
@@ -304,9 +300,12 @@ module csr(
         end
     end
 
+    assign csr_tval_data  = timer_cnt[31:0];
+
     // TICLR的CLR域
     assign csr_ticlr_clr = 1'b0;
 
+    // CSR 的读出逻辑
     assign csr_crmd_data  = {23'b0, csr_crmd_datm, csr_crmd_datf, csr_crmd_pg, 
                             csr_crmd_da, csr_crmd_ie, csr_crmd_plv};
     assign csr_prmd_data  = {29'b0, csr_prmd_pie, csr_prmd_pplv};
@@ -316,7 +315,6 @@ module csr(
     assign csr_badv_data  = csr_badv_vaddr;
     assign csr_tid_data   = csr_tid_tid;
     assign csr_tcfg_data  = {csr_tcfg_initval, csr_tcfg_periodic, csr_tcfg_en};
-    assign csr_tval_data  = timer_cnt;
     assign csr_ticlr_data = {31'b0, csr_ticlr_clr};
     assign csr_rvalue = {32{csr_num == `CSR_CRMD  }} & csr_crmd_data
                       | {32{csr_num == `CSR_PRMD  }} & csr_prmd_data
